0b25447612152e265b4cfaee240bbf5ea4de0db8
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-11-10 21:06:08 +0530

    added intial files

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+/*=============== SEARCH ===============*/
+const searchButton = document.getElementById('search-button'),
+searchClose = document.getElementById('search-close'),
+searchContent = document.getElementById('search-content');
+
+if(searchButton){
+    searchButton.addEventListener('click',()=>{
+        searchContent.classList.add('show-search')
+    })
+}
+
+if(searchClose){
+    searchClose.addEventListener('click',()=>{
+        searchContent.classList.remove('show-search');
+    })
+}
+
+
+
+/*=============== LOGIN ===============*/
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+
+/*=============== HOME SWIPER ===============*/
+
+
+/*=============== FEATURED SWIPER ===============*/
+
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
