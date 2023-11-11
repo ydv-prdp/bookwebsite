6eb7842a6a023414549ba345346912f16c8b7e2a
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-11-11 14:18:12 +0530

    testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,19 @@ let swiperNew = new Swiper('.new__swiper',{
 })
 
 /*=============== TESTIMONIAL SWIPER ===============*/
-
+let swiperTestimonials = new Swiper('.testimonial__swiper',{
+    loop:true,
+    spaceBetween:16,
+    grabCursor:true,
+    slidesPerView:'auto',
+    centeredSlides:'auto',
+    breakpoints:{
+        1150:{
+            slidesPerView:3,
+            centeredSlides:false
+        }
+    }
+})
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
