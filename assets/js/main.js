2e2580f2bf69ac26a4f41ac75b3507fef57d017b
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-11-11 15:18:39 +0530

    added scroll up css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,7 +115,12 @@ let swiperTestimonials = new Swiper('.testimonial__swiper',{
 })
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = ()=>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
